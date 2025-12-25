0cd511dbe05b3e5c562b3affb5d453fc9514fe66
Facundo Tom <facu_tomasella@hotmail.com>
2025-12-25 17:45:57 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,4 @@
-const CACHE_NAME = 'tradingchango-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  // Agrega aquí tus archivos CSS o imágenes principales si los tienes por separado
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+// Service Worker mínimo para que la App sea instalable
+self.addEventListener('fetch', (event) => {
+  // Solo necesita existir para que el navegador lo detecte
 });
